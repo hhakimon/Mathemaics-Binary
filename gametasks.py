508bd51f4d5fc3a0120a781e4428693021be6a1c
hhakimon <9116098@gmail.com>
2025-01-01 23:48:25 +0500

    qizla

--- gametasks.py
+++ gametasks.py
@@ -37,5 +37,5 @@ def getUserScore(userName):
             
             remove('userScores.txt')
             rename('userScores.tmp', 'userScores.txt')
-            #салом болла
+            #салом болла ва кизла
             
