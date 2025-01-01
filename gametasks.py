99527c5bc4670f9896a420cb336b34800be84f18
hhakimon <9116098@gmail.com>
2025-01-01 23:20:48 +0500

    q

--- gametasks.py
+++ gametasks.py
@@ -1,3 +1,18 @@
 def printInstructions(instruction):
     print(instruction)
-    
+
+def getUserScore(userName):
+    try:
+        input = open('userScores.txt', 'r')
+        for line in input:
+            content = line.split(', ')
+            if content[0] == userName:
+                input.close()
+                return content[ 1]
+            input.close()
+            return '-1'
+    except IOError:
+        print("File not found. A new file will be created.")
+        input = open('userScores.txt', 'w')
+        input.close()
+        return '-1'
