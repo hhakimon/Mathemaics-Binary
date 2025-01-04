d667cb73f5103b42033f87d9f631bf084dd97eaa
hhakimon <9116098@gmail.com>
2025-01-04 09:48:14 +0500

    УПРАЖНЕНИЕ 2.2

--- gameclasses.py
+++ gameclasses.py
@@ -11,10 +11,38 @@ class Game:
         if value < 1:
             self._noOfQuestions = 1
             print("\nMinimum Number of Questions = 1")
-            print("Hence, number of questions will be setto 1")
+            print("Hence, number of questions will be set to 1")
         elif value > 10:
             self._noOfQuestions = 10
             print("\nMaximum Number of Questions = 10")
-            print("Hence, number of questions will be setto 10")
+            print("Hence, number of questions will be set to 10")
         else:
             self._noOfQuestions = value
+
+class BinaryGame(Game):
+    def generateQuestions(self):
+        from random import randint
+        score = 0
+
+        for i in range(self.noOfQuestions):
+            base10 = randint(1, 100)
+            userResult = input("\nPlease convert %d tobinary: " %(base10))
+            while True:
+                try:
+                    answer = int(userResult, base = 2)
+                    if answer == base10:
+                        print("Correct Answer!")
+                        score = score + 1
+                        break
+                    else:
+                        print("Wrong answer. The correct answer"\
+                             " is {:b}.".format(base10))
+                        break
+                except:
+                    print("You did not enter a binary number.Please try again.")
+                    userResult = input("\nPlease convert %d tobinary: " %(base10))
+
+        return score
+    
+                        
+                        
