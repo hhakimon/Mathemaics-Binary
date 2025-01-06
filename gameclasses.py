56eeb5a7a833a4756e037a0bb7188cb670f47cb3
hhakimon <9116098@gmail.com>
2025-01-06 22:34:54 +0500

    УПРАЖНЕНИЕ 2.3

--- gameclasses.py
+++ gameclasses.py
@@ -26,7 +26,7 @@ class BinaryGame(Game):
 
         for i in range(self.noOfQuestions):
             base10 = randint(1, 100)
-            userResult = input("\nPlease convert %d tobinary: " %(base10))
+            userResult = input("\nPlease convert %d to binary: " %(base10))
             while True:
                 try:
                     answer = int(userResult, base = 2)
@@ -43,6 +43,72 @@ class BinaryGame(Game):
                     userResult = input("\nPlease convert %d tobinary: " %(base10))
 
         return score
-    
-                        
-                        
+
+class MathGame(Game):
+    def generateQuestions(self):
+        from random import randint
+        score = 0
+        numberList = [0, 0, 0, 0, 0]
+        symbolList = ['', '', '', '']
+        operatorDict = {1:' + ', 2:' - ', 3:'*', 4:'**'}
+
+        for i in range(self.noOfQuestions):
+            for index in range(0, 5):
+                numberList[index] = randint(1, 9)
+# См. объяснение ниже
+            for index in range(0, 4):
+                if index > 0 and symbolList[index - 1] == '**':
+                    symbolList[index] = operatorDict[randint(1, 3)]
+
+                else:
+                    symbolList[index] = operatorDict[randint(1, 4)]
+
+            questionString = str(numberList[0])
+            
+            for index in range(0, 4):
+                questionString = questionString + symbolList[index] + str(numberList[index+1])
+
+            result = eval(questionString)
+            questionString = questionString.replace("**", "^")
+            userResult = input("\nPlease evaluate %s:"%(questionString))
+
+            while True:
+                try:
+                    answer = int(userResult)
+                    if answer == result:
+                        print("Correct Answer!")
+                        score = score + 1
+                        break
+                    else:
+                        print("Wrong answer. The correct answer is {:d}.".format(result))
+                        break
+                except:
+                    print("You did not enter a valid number. Please try again.")
+                    userResult = input("\nPlease evaluate %s:"%(questionString))
+
+        return score 
+                     
+                       
+'''
+ Начиная со второго элемента (т. е. index = 1) 
+в symbolList, строка
+ if index > 0 and symbolList[index-1] == '**': 
+проверяет, содержит ли
+ предыдущий элемент в symbolList оператор **.
+ Если содержит, то команда symbolList[index] = 
+operatorDict[randint(1, 3)]
+ выполняется. В этом случае функции randint передается 
+диапазон от 1 до 3.
+ Таким образом, оператор **, имеющий ключ 4 
+в operatorDict, НЕ БУДЕТ
+ присвоен symbolList[index].
+ С другой стороны, если предыдущий элемент не содержит 
+оператор **, команда
+ symbolList[index] = operatorDict[randint(1, 4)] будет 
+выполнена. Так как
+ функции randint передается диапазон от 1 до 4, будут 
+сгенерированы числа
+ из набора 1, 2, 3 и 4. А значит, symbolList[index] 
+будут присваиваться 
+операторы +, -, * и **.
+ '''
